cade1480d7c73df0adf16ab74542c0419fc6496c
Marco Avila <marco.aviladev@gmail.com>
2025-12-05 14:55:51 -0600

    Primer Deploy

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (require, res) => {
+    res.send("hello world!");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
